1ecbe14b7cffd7c2afee1d5a63d1708274a92d09
KarlaReyes08 <ayuminadoka102@gmail.com>
2023-03-20 14:16:59 -0600

    Update MainActivity.java
    prueba

--- Farmacias/app/src/main/java/sv/edu/farmacias/MainActivity.java
+++ Farmacias/app/src/main/java/sv/edu/farmacias/MainActivity.java
@@ -12,5 +12,5 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    // Hola prueba
+    // Hola prueba como estan grupo esta es una prueba de karla
 }
