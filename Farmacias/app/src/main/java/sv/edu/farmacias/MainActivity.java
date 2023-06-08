56f57608162a81cf854b1ed22f30da59af63dec2
Davis Cruz <46834431+davicbaba@users.noreply.github.com>
2023-06-08 08:13:56 -0600

    avance de interfaces

--- Farmacias/app/src/main/java/sv/edu/farmacias/MainActivity.java
+++ Farmacias/app/src/main/java/sv/edu/farmacias/MainActivity.java
@@ -2,7 +2,9 @@ package sv.edu.farmacias;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +14,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    // Hola prueba
+    public void OnClickSearch(View view){
+        Intent intent = new Intent(this, Search.class);
+        startActivity(intent);
+    }
 
 }
---
+++ Farmacias/app/src/main/java/sv/edu/farmacias/Search.java
@@ -0,0 +1,46 @@
+package sv.edu.farmacias;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.SearchView;
+
+public class Search extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu, menu);
+
+        MenuItem menuItem = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) menuItem.getActionView();
+        searchView.setQueryHint("Type here to search");
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener()
+        {
+
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+
+
+        return super.onCreateOptionsMenu(menu);
+    }
+}
